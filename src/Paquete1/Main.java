16e885ce6889f06c55f8732053227ce371998760
Pablo Vila Mauricio <pvilamauricio@danielcastelao.org>
2021-12-17 10:45:49 +0100

    Añadido un sout

--- src/Paquete1/Main.java
+++ src/Paquete1/Main.java
@@ -1,8 +1,10 @@
 package Paquete1;
 
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        System.out.println("Hello World");
     }
 }
