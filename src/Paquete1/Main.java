64bf2857211032964a093ed1541f80c47fe20a95
Pablo Vila Mauricio <pvilamauricio@danielcastelao.org>
2021-12-17 10:46:38 +0100

    Añadido el ultimo sout

--- src/Paquete1/Main.java
+++ src/Paquete1/Main.java
@@ -6,6 +6,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
         System.out.println("Hello World");
-        System.out.println("Añadido otro Sout para demostrar que se hacer commits");
+        System.out.println("Añadido otro Sout para demostrar que se hacer commitss");
     }
 }
